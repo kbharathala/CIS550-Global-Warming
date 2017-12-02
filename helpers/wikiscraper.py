b3a6adc21fe976627c6bda6fd317ce8ba3fb0815
Alec K Wang <alecwang@eniac.seas.upenn.edu>
2017-12-02 17:19:40 -0500

    adds description, population, land area, gdp, gini, hdi to wikiscraper

--- helpers/wikiscraper.py
+++ helpers/wikiscraper.py
@@ -1,12 +1,32 @@
-from bs4 import BeautifulSoup
-import urllib2
+import requests
+import re
+from bs4 import BeautifulSoup as bs
+
+
+def strip_brackets(text):
+  text = re.sub('\[[^\]]+\]', '', text)
+  return text
+
+
+def to_numeric(text):
+  text = strip_brackets(text)
+  text = re.sub('\([^\)]+\)', '', text)
+  multiplier = re.sub('[^a-z]', '', text)
+  number = float(re.sub('[^\\d^.]', '', text))
+  if multiplier == 'trillion':
+    number = number * 1000000000000
+  elif multiplier == 'billion':
+    number = number * 1000000000
+  elif multiplier == 'million':
+    number = number * 1000000
+  return number
+
 
 def get_infobox(country):
   site = "http://en.wikipedia.org/wiki/" + country
-  req = urllib2.Request(site, headers = {'User-Agent': 'Mozilla/5.0'})
-  page = urllib2.urlopen(req)
+  r = requests.get(site)
 
-  soup = BeautifulSoup(page.read(), "lxml")
+  soup = bs(r.text, "lxml")
   try:
     table = soup.find('table', class_='infobox geography vcard')
   except Exception as e:
@@ -14,13 +34,29 @@ def get_infobox(country):
     return
 
   result = {}
+  pop = table.find('th', string='Population').find_parent().find_next_sibling().find('td').text
+  result['Population'] = int(to_numeric(pop))
+  area = table.find('th', string='Area').find_parent().find_next_sibling().find('td').text
+  result['Land Area'] = int(to_numeric(area))
+  gdp = table.find('a', string='GDP').find_parent().find_parent().find_next_sibling().find('td').text
+  result['GDP'] = int(to_numeric(gdp))
+  gini = table.find('a', string='Gini').find_parent().find_next_sibling().text
+  result['Gini'] = to_numeric(gini)
+  hdi = table.find('a', string='HDI').find_parent().find_next_sibling().text
+  result['HDI'] = to_numeric(hdi)
+  desc = soup.find('b', string=country).find_parent().text
+  result['Description'] = strip_brackets(desc)
+
+  """
+  result = {}
   for tr in table.find_all('tr'):
     if tr.find('th'):
       try: 
         result[tr.find('th').text.rstrip().lstrip()] = tr.find('td').text.rstrip()
       except:
         print('failed')
+  """
     
   return result
 
-print(get_infobox("india"))
+print(get_infobox("India"))
