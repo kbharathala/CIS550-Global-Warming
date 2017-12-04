c63669b4b257110ffe88dadc9d7d675b5a33e892
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2017-12-04 00:40:35 -0500

    Changed main import

--- app/__init__.py
+++ app/__init__.py
@@ -38,7 +38,7 @@ def create_app(config_name):
         SSLify(app)
 
     # Create app blueprints
-    from main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
 
